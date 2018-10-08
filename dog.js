9bdb9845891a3c68a6b6878350d10729457c6aff
Tuannh2210 <tuannh2210@gmail.com>
2018-10-08 08:57:27 +0700

    Add dog method

--- dog.js
+++ dog.js
@@ -1,2 +1,9 @@
-function dog(argument) {
+var chalk = require('chalk');
+function Dog(name) {
+	this.name = name;
 }
+Dog .prototype.sayHi = function(){
+	console.log('Hi '+chalk.red(this.name));
+}
+
+module.exports = Dog
